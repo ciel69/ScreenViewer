1b2b08bc63f2cd17a2538e122b7c77f8223a6129
bukhaltsov <bukhaltsov@componetix.ru>
2017-11-07 11:54:15 +0300

    create class ScreenViewer

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import screenViewer from 'screenViewer';
+import {Observable} from 'rxjs/Rx';
+
+export default class ScreenViewer {
+    constructor(map, event) {
+        if (typeof map !== 'undefined' && map !== null) {
+            screenViewer.setup(map);
+        }
+
+        const targetEventList = [
+            Observable.fromEvent(window, 'load').map(() => window.innerWidth),
+            Observable.fromEvent(document, 'DOMContentLoaded').map(event => event.target.innerWidth),
+            Observable.fromEvent(window, 'resize').map(() => window.innerWidth),
+        ];
+
+        if (typeof event !== 'undefined' && event !== null) {
+            targetEventList.push(...event);
+        }
+
+        return screenViewer.init$(targetEventList);
+    }
+}
